ba70e541e226b8f63b86723e676102ab63f693b3
Trevor Bakker <trevor.bakker@uta.edu>
2019-03-19 02:43:53 +0000

    Initial submission of comparison code

---
+++ compare.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+  char IMG_Name[11] = "FOO     TXT";
+
+  char input[7] = "foo.txt";
+
+  char expanded_name[12];
+  memset( expanded_name, ' ', 12 );
+
+  char *token = strtok( input, "." );
+
+  strncpy( expanded_name, token, strlen( token ) );
+
+  token = strtok( NULL, "." );
+
+  if( token )
+  {
+    strncpy( (char*)(expanded_name+8), token, strlen(token ) );
+  }
+
+  expanded_name[11] = '\0';
+
+  int i;
+  for( i = 0; i < 11; i++ )
+  {
+    expanded_name[i] = toupper( expanded_name[i] );
+  }
+
+  if( strncmp( expanded_name, IMG_Name, 11 ) == 0 )
+  {
+    printf("They matched\n");
+  }
+
+  return 0;
+}
